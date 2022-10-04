c336a67a1f9d120d8fffc5e6d508062aa3f5cff3
mahmoud mohammed <mahmoud.mohammed@ivas.com.eg>
2022-10-04 12:30:34 +0200

    fix include js files path

--- src/views/messenger/index.blade.php
+++ src/views/messenger/index.blade.php
@@ -8,14 +8,14 @@
         <title>Messenger</title>
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="{{ asset('/') }}/assets/messenger/images/icon.png" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('/') }}assets/messenger/images/icon.png" type="image/x-icon">
 
         <!-- Template CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         
-        <link rel="stylesheet" href="{{ asset('/') }}/assets/messenger/css/template.bundle.dark.css">
-        <link rel="stylesheet" href="{{ asset('/') }}/assets/messenger/css/template.dark.bundle.css" media="(prefers-color-scheme: dark)">
-        <link rel="stylesheet" href="{{ asset('/') }}/assets/messenger/css/style.css">
+        <link rel="stylesheet" href="{{ asset('/') }}assets/messenger/css/template.bundle.dark.css">
+        <link rel="stylesheet" href="{{ asset('/') }}assets/messenger/css/template.dark.bundle.css" media="(prefers-color-scheme: dark)">
+        <link rel="stylesheet" href="{{ asset('/') }}assets/messenger/css/style.css">
 
     <body>
         <!-- Layout -->
@@ -34,19 +34,19 @@
         @include('messenger.includes.modals')
 
         @vite(['resources/js/app.js'])
-        {{-- <script type="text/javascript" src="{{ asset('/') }}/js/app.js"></script> --}}
+        {{-- <script type="text/javascript" src="{{ asset('/') }}js/app.js"></script> --}}
         <script type="text/javascript">
             const AUTH_USER_ID    = {{ auth()->id() }};
-            const audio           = new Audio(`{{ asset('/') }}/assets/messenger/audios/success.mp3`);
+            const audio           = new Audio(`{{ asset('/') }}assets/messenger/audios/success.mp3`);
             const IMG_COLUMN_NAME = "{{ asset('/') }}";
             const APPEND_URL      = "{{ asset('/') }}";
             const DEFAULT_IMG     = "{{ asset('/') }}";
             Pusher.logToConsole   = "{{ asset('/') }}";
         </script>
-        <script type="text/javascript" src="{{ asset('/') }}/assets/messenger/js/jquery-3.6.1.min.js"></script>
-        <script type="text/javascript" src="{{ asset('/') }}/assets/messenger/js/vendor.js"></script>
-        <script type="text/javascript" src="{{ asset('/') }}/assets/messenger/js/template.js"></script>
-        <script type="text/javascript" src="{{ asset('/') }}/assets/messenger/js/moment.js"></script>
-        <script type="text/javascript" src="{{ asset('/') }}/assets/messenger/js/messenger.js"></script>
+        <script type="text/javascript" src="{{ asset('/') }}assets/messenger/js/jquery-3.6.1.min.js"></script>
+        <script type="text/javascript" src="{{ asset('/') }}assets/messenger/js/vendor.js"></script>
+        <script type="text/javascript" src="{{ asset('/') }}assets/messenger/js/template.js"></script>
+        <script type="text/javascript" src="{{ asset('/') }}assets/messenger/js/moment.js"></script>
+        <script type="text/javascript" src="{{ asset('/') }}assets/messenger/js/messenger.js"></script>
     </body>
 </html>
