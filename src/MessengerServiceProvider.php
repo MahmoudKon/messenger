18e1057998a976a4e91569a9334838091f4531e7
mahmoud mohammed <mahmoud.mohammed@ivas.com.eg>
2022-10-05 15:39:43 +0200

    fix image url

--- src/MessengerServiceProvider.php
+++ src/MessengerServiceProvider.php
@@ -20,15 +20,17 @@ class MessengerServiceProvider extends ServiceProvider
 
     public function boot()
     {        
+        $this->loadViewsFrom(__DIR__.'/views', resource_path('views'));
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadRoutesFrom(__DIR__.'/routes/channels.php');
-        $this->loadMigrationsFrom(__DIR__.'/migrations');
-
-        $this->loadViewsFrom(__DIR__.'/views/messenger', 'messenger');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/views');
 
         $this->publishes([
             __DIR__.'/config/messenger.php' => config_path('messenger.php'),
             __DIR__.'/assets/messenger' => public_path('assets/messenger'),
+            __DIR__.'/migrations' => database_path('migrations'),
+            __DIR__.'/views' => resource_path('views'),
         ], 'messenger');
     }
 }
