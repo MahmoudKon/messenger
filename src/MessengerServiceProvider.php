7ec33cecaef2d93eb11d926c201b457a23d55c41
MahmoudK0n <mahmoud.mohammed@ivas.com.eg>
2022-10-06 19:51:58 +0200

    add event name in config

--- src/MessengerServiceProvider.php
+++ src/MessengerServiceProvider.php
@@ -14,26 +14,41 @@ class MessengerServiceProvider extends ServiceProvider
                 MessengerCommand::class
             ]);
         }
+
+        $this->mergeConfigFrom(
+            __DIR__.'/config/messenger.php', 'messenger'
+        );
     }
 
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/resources/views', 'messenger');
-        $this->loadRoutesFrom(__DIR__.'/routes/channels.php');
+        // $this->loadRoutesFrom(__DIR__.'/routes/channels.php');
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-
         $this->loadMigrationsFrom(__DIR__.'/migrations');
 
         $this->publishes([
-            __DIR__.'/config/messenger.php' => config_path('messenger.php'),
-            __DIR__.'/assets/messenger' => public_path('assets/messenger'),
+            __DIR__.'/config' => config_path('/'),
             __DIR__.'/views' => resource_path('views'),
             __DIR__.'/assets' => public_path('assets'),
+            __DIR__.'/migrations' => database_path('migrations'),
         ], 'messenger');
 
         $this->publishes([
             __DIR__.'/migrations' => database_path('migrations'),
         ], 'messenger-migrations');
+
+        $this->publishes([
+            __DIR__.'/config' => config_path('/'),
+        ], 'messenger-config');
+
+        $this->publishes([
+            __DIR__.'/views' => resource_path('views'),
+        ], 'messenger-views');
+
+        $this->publishes([
+            __DIR__.'/assets' => public_path('assets'),
+        ], 'messenger-assets');
     }
 }
 
