95ff540f66d96a73de7316b2e119746734bb26fc
MahmoudK0n <mahmoud.mohammed@ivas.com.eg>
2022-10-06 21:44:24 +0200

    add event name in config

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,20 +5,16 @@ use Messenger\Chat\Controllers\ConversationController;
 use Messenger\Chat\Controllers\MessageController;
 
 Route::middleware(config('messenger.middleware'))->prefix(config('messenger.route_prefix'))->group(function() {
-    Route::controller(ConversationController::class)->group(function () {
-        Route::get('/', 'index')->name('messenger');
-        Route::get('users', 'users')->name('users');
-        Route::get('update/last-seen', 'updateLastSeen')->name('conversations.updateLastSeen');
-        Route::get('user/{user}/details', 'userDetails')->name('user.details');
-        Route::delete('conversation/{conversation}/destroy', 'destroy')->name('conversation.destroy');
-        Route::get('single/conversation/{id}', 'singleConversation')->name('conversation.single.conversation');
-    });
+    Route::get('/', [ConversationController::class, 'index'])->name('messenger');
+    Route::get('users', [ConversationController::class, 'users'])->name('users');
+    Route::get('update/last-seen', [ConversationController::class, 'updateLastSeen'])->name('conversations.updateLastSeen');
+    Route::get('user/{user}/details', [ConversationController::class, 'userDetails'])->name('user.details');
+    Route::delete('conversation/{conversation}/destroy', [ConversationController::class, 'destroy'])->name('conversation.destroy');
+    Route::get('single/conversation/{id}', [ConversationController::class, 'singleConversation'])->name('conversation.single.conversation');
 
-    Route::controller(MessageController::class)->group(function () {
-        Route::get('conversation/{user}/messages', 'index')->name('conversation.user.messages');
-        Route::get('conversation/{conversation}/messages/load-more', 'getMessages')->name('conversation.load.messages');
-        Route::post('messages', 'store')->name('message.store');
-        Route::get('update/read-at', 'updateReadAt')->name('messages.updateReadAt');
-        Route::post('message/{message_id}/delete/{user_id?}', 'delete')->name('messages.delete');
-    });
+    Route::get('conversation/{user}/messages', [MessageController::class, 'index'])->name('conversation.user.messages');
+    Route::get('conversation/{conversation}/messages/load-more', [MessageController::class, 'getMessages'])->name('conversation.load.messages');
+    Route::post('messages', [MessageController::class, 'store'])->name('message.store');
+    Route::get('update/read-at', [MessageController::class, 'updateReadAt'])->name('messages.updateReadAt');
+    Route::post('message/{message_id}/delete/{user_id?}', [MessageController::class, 'delete'])->name('messages.delete');
 });
