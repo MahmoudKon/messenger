d4176ed46df4305d9b8e88245bf33ca5e8250608
mahmoud mohammed <mahmoud.mohammed@ivas.com.eg>
2022-10-09 10:17:27 +0200

    fix image url

--- src/assets/messenger/js/messenger.js
+++ src/assets/messenger/js/messenger.js
@@ -372,7 +372,7 @@ $(function() {
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li>
-                                        <a class="dropdown-item d-flex align-items-center text-danger remove-conversation-message" data-message-id="${message.id}" href="message/${message.id}/delete">
+                                        <a class="dropdown-item d-flex align-items-center text-danger remove-conversation-message" data-message-id="${message.id}" href="message/${message.id}/delete/${AUTH_USER_ID}">
                                             <div class="icon">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
                                             </div>
@@ -381,7 +381,7 @@ $(function() {
                                     </li>`;
             if (message.user_id == AUTH_USER_ID) {
                 content += `<li>
-                                <a class="dropdown-item d-flex align-items-center text-danger remove-from-all remove-conversation-message" data-message-id="${message.id}" href="message/${message.id}/delete/${AUTH_USER_ID}">
+                                <a class="dropdown-item d-flex align-items-center text-danger remove-from-all remove-conversation-message" data-message-id="${message.id}" href="message/${message.id}/delete">
                                     <div class="icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
                                     </div>
